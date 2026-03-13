515ecbb6c13f6f0020fc3bb88039166bf215934c
J Sebastian <j.echeverry3@utp.edu.co>
2026-03-13 01:38:29 -0500

    Finalización del ejercicio 4 v1.0

--- tallerCiclos/taller_ejercicio4.py
+++ tallerCiclos/taller_ejercicio4.py
@@ -1,19 +1,22 @@
 while True:
     print("\nRegistrando hora...")
-    t = (input("\n¿Son las 6 A.M.? "))
+    t = (input("¿Son las 6 A.M.? "))
     if t == "no" :
         print("\nAun no suena la alarma")
     elif t == "si" :
         print("\n¡Sonando alarma!")
         b = 0
-        while b < 3:
-            Apag = input("\n¿Se ha apagado la alarma? ")
+        while b < 60:
+            Apag = input("¿Se ha apagado la alarma? ")
             if Apag == "no" :
                 b = b + 1
-                if b != 3:
+                if b != 60:
                     print("\n¡Sigue sonando la alarma!")
-                elif b == 3:
+                elif b == 60:
                     print("\nApagando despertador")
             elif Apag == "si" :
-                print("\nApagando despertador")
-                b = 3
+                print("Apagando despertador")
+                b = 60
+    elif t == "desactivar":
+        print("\n---Alarma desactivada---\n")
+        break
