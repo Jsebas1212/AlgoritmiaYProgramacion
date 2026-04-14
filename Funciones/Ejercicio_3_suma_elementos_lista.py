b683ec9d35bf007f180c6b7921fec6d0bc698665
J Sebastian <j.echeverry3@utp.edu.co>
2026-04-13 22:03:42 -0500

    Ejercicio 3 v1.1

--- Funciones/Ejercicio_3_suma_elementos_lista.py
+++ Funciones/Ejercicio_3_suma_elementos_lista.py
@@ -1,8 +1,31 @@
-def suma_recursiva(lista):
+"""
+Este código lo que hace es tomar cada uno de los elementos de
+una lista ingresada por el usuario e imprimir el resultado de
+esos elementos.
+
+prompt: Generame una función en python donde yo le de una lista
+de números y me devuelva la suma de todos sus elementos y explicame
+como funciona esa función valga la redundancia.
+
+Johan Sebastian Echeverry Vega
+c.c. 1113782839
+Universidad Tecnológica de Pereira
+Fecha: 2026/04/13
+"""
+def sumalist(lista):
+    """ 
+    Devuelve los elementos de una lista y los va sumando
+
+    Parámetros:
+    lista (list): Lista de números reales
+
+    Retorna:
+    float: La suma de los elementos de la lista ingresada
+    """
     if len(lista) == 0:
         return 0
     else:
-        return lista[0] + suma_recursiva(lista[1:])
+        return lista[0] + sumalist(lista[1:])
 
 print("---Hey muy buenas a todos, wapisimos!!!---")
 print("---Aquí Vegetta 777 en un nuevo directo de Planeta Vegetta!!!---")
@@ -11,8 +34,8 @@ while True:
     option = input("\nPresiona enter para continuar o escribe 'salir' para cerrar el progranma: ")
     if option == "":
         while True:
-            numlist = input("\nEscribe una lista de números, ¡OJO! separados por espacios: ")
-            if numlist == "salir":
+            numlist = input("\nEscribe una lista de números, ¡OJO! separados por espacios: ").lower()
+            if numlist == "salir" or numlist == "s":
                 print("Saliendo...")
                 exit()
             else:
@@ -20,7 +43,7 @@ while True:
                 try: 
                     for o in range(len(List1)):
                         List1[o] = float(List1[o])
-                    print("\nLa suma de los elementos de la lista ingresada es: ", suma_recursiva(List1))
+                    print("\nLa suma de los elementos de la lista ingresada es: ", sumalist(List1))
                 except ValueError:
                     print("\nPinche bruto, ¿no ves que solo puedes ingresar números? Intenta de nuevo ingeniero industrial.")
     elif option.lower() == "salir" or option.lower()== "s":
