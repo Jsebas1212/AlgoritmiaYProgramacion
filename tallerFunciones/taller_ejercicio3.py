65a0c2ddbdd2deaf3b773b64dc74b5c1de0d3fd9
J Sebastian <j.echeverry3@utp.edu.co>
2026-04-14 23:09:03 -0500

    Actualización al ejercicio 3, agregados mayores y dejo cosas pendientes para hacer luego

--- tallerFunciones/taller_ejercicio3.py
+++ tallerFunciones/taller_ejercicio3.py
@@ -21,7 +21,64 @@ def calcular_valor_total(diccionario):
 def mostrar_inventario(diccionario):
     print("---Inventario actual---")
     for nombre in diccionario:
-        cantidad = diccionario[nombre][cantidad]
-        precio = diccionario[nombre][precio]
+        cantidad = diccionario[nombre]["cantidad"]
+        precio = diccionario[nombre]["precio"]
         print(f"{nombre} : {cantidad} : por unidad ${precio}")
 
+
+
+print("\nBienvenido al sistema de inventario")
+print("¿Desea empezar con un inventario ya definido o desea crear uno desde cero? (D/C)")
+
+while True:
+    while True:
+        pregunta0 = input("")
+        if pregunta0.lower() == "d":
+            inventario = {
+                            "Manzana": {"cantidad": 50, "precio": 1200},
+                            "Leche": {"cantidad": 20, "precio": 4500},
+                            "Pan": {"cantidad": 15, "precio": 2500},
+                            "Cafe": {"cantidad": 10, "precio": 15000},
+                            "Huevos": {"cantidad": 30, "precio": 600}
+                            }
+            print("Su inventario de prueba es el siguiente: ")
+            print(f"\n{mostrar_inventario(inventario)}")
+            break
+        elif pregunta0.lower() == "c" :
+            inventario = {}
+            break
+        else:
+            print("Por favor ingrese una opción válida")
+
+    while True:
+        print("\n¿Qué desea hacer? ")
+        print("""
+            Para agregar un producto nuevo al inventario escriba...."agregar"  o "A"
+            Para eliminar un producto del inventario escriba........"eliminar" o "E"
+            Para Calcular el valor total del inventario escriba....."calcular" o "C"
+            Para ver el inventario escriba.........................."mostrar"  o "M"
+            Para salir del programa escriba........................."salir"    o "S"
+                    """)
+        actionsinv = input("")
+        if actionsinv.lower() in ["agregar","a"]:
+            print("Escriba lo siguiente en ese orden separado por espacios")
+            print("\nNombre del producto - cantidad de unidades del producto - precio del producto")
+            agrlista = input("\n")
+            datos = agrlista.split()
+            nombre = datos[0]
+        
+        # 2. Súper importante: convertir el texto a números
+        cantidad = int(datos[1])
+        precio = float(datos[2])
+        
+        # 3. Ahora sí llamamos a la función con sus 4 variables separadas.
+        # OJO: Cambia 'mi_inventario' por el nombre real que le pusiste a tu diccionario al inicio.
+        agregar_producto(inventario, nombre, cantidad, precio)
+
+        # elif actionsinv.lower in ["eliminar","e"]:
+
+        # elif actionsinv.lower in ["calcular","c"]:
+
+        # elif actionsinv.lower in ["mostrar","m"]:
+        
+        # elif actionsinv.lower in ["salir","s"]:
