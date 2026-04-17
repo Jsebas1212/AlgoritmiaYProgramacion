6c01c4bed279dc8de3984598225eb63a749f319a
J Sebastian <j.echeverry3@utp.edu.co>
2026-04-17 13:36:04 -0500

    Ejercicio 6 v1.0

--- tallerFunciones/taller_ejercicio6.py
+++ tallerFunciones/taller_ejercicio6.py
@@ -1,3 +1,16 @@
+def mostrar_productos(diccionario):
+    for nombre, precio in diccionario.items():
+        print(f"|{nombre}| : Precio del producto ${precio}")
+
+def verificar_dinero(precio, saldo_total):
+    if saldo_total < precio:
+        faltante = precio - saldo_total
+        print(f"\nEl dinero no es suficiente. Te faltan ${faltante}\n")
+        return False
+    else:
+        return True 
+    
+
 productos = {
     "Doritos": 4000,
     "Monster": 7000,
@@ -6,42 +19,47 @@ productos = {
     "Oreo": 3000
             }
 
-def mostrar_productos(diccionario):
-    for nombre, precio in diccionario.items():
-        print(f"|{nombre}| : Precio del producto ${precio}")
+print("\n¡HOLA¡ Bienvenido a la máquina dispensadora!\n")
 
-def verificar_dinero(precio, pago):
-    if pago < precio:
-        faltante = precio - pago
-        print(f"El dinero no es suficiente. Te faltan ${faltante}.")
-        return False
+while True:
+    print("\nSeleccione el producto que desee:")
+    mostrar_productos(productos)
+    
+    producto_elegido = input("\n¿Qué producto elige? ").capitalize()
+
+    if producto_elegido in productos:
+        precio_producto = productos[producto_elegido]
+        print(f"\nProducto elegido : {producto_elegido}")
+        print(f"Total a pagar:     ${precio_producto}")
+        
+        saldo = 0
+        
+        while saldo < precio_producto:
+            try:
+                pago_usuario = float(input("\nIngrese la cantidad de dinero con la que va a pagar: $"))
+                saldo = pago_usuario + saldo
+            except ValueError:
+                print("\nPor favor ingrese el valor numérico de la cantidad de dinero con la que va a pagar.")
+                continue
+
+            if verificar_dinero(precio_producto, saldo) == True:
+
+                devuelta = saldo - precio_producto
+            
+                if devuelta > 0:
+                    print(f"\n¡Compra exitosa! Entregando {producto_elegido}...")
+                    print(f"\nTu devuelta es: ${devuelta} \nPor favor retírela")
+                    print("\nQue tenga un buen día usuario")
+                    input("\n\nVolviendo al menú de opciones")
+
+
+                else:
+                    print(f"\n¡Compra exitosa! Entregando {producto_elegido}...")
+                    print("\nPagaste exacto, no hay devuelta.\n")
+                    print("\nQue tenga un buen día usuario")
+                    input("\n\nPresiona 'enter' para volver al menú de opciones")
+    elif producto_elegido in ["Apagar","Salir","S","A"]:
+        print("\nApagando máquina dispensadora...")
+        break
     else:
-        return True 
-
-
-print("\n¡Bienvenido a la máquina dispensadora!\n")
-print("Seleccione el producto que desee:")
-mostrar_productos(productos)
-
-producto_elegido = input("\n¿Qué producto elige? ").capitalize()
-
-if producto_elegido in productos:
-    precio_producto = productos[producto_elegido]
-    print(f"\nProducto elegido : {producto_elegido}")
-    print(f"Total a pagar:     ${precio_producto}")
-    
-    pago_usuario = int(input("\nIngrese la cantidad de dinero con la que va a pagar: $"))
-    
-    if verificar_dinero(precio_producto, pago_usuario):
-        
-        devuelta = pago_usuario - precio_producto
-        
-        if devuelta > 0:
-            print(f"\n¡Compra exitosa! Entregando {producto_elegido}...")
-            print(f"Tu devuelta es: ${devuelta}")
-        else:
-            print(f"\n¡Compra exitosa! Entregando {producto_elegido}...")
-            print("Pagaste exacto, no hay devuelta.")
-
-else:
-    print("\nProducto no válido. Por favor reinicie la máquina.")
+        print("\nProducto no válido. Por favor digite correctamente el nombre del producto que desea.\n")
